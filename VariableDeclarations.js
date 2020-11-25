c997a79ae180c5fb4bdc4d2b1f91211d40797e1f
Sheprata Shrestha <i82463@d2hs.com>
2020-11-25 13:07:33 +0545

    Updated file VariableDeclarations.js

--- VariableDeclarations.js
+++ VariableDeclarations.js
@@ -2,22 +2,19 @@
 
 /*multiline*/
 // variable declaration in dynamic type programming language is implict - python ruby javascript
+//something we dont get in static type language like java and C#, can't assign all kind of value in same variable
 
-//primary type
-var x = 'sheprata'; //takes  single quotes as well
-var y = 89;
+//Primary type
+var x = 'Sheprata'; //takes single quote as well
+var e = 89;
 var d = "stringtoo";
 var c = true ;
-var y = 2n ** 200n; //large number - bigint
+var y = 2n ** 200n; //large number - bigint is defined with 'n'
 var a = undefined;
 var b = null;
 
 //structural type like objects, arrays
-//arrays of number
+//g - array of number
 var g = [2,3,4,5];
-//h - arrays of object
+//h - array of object
 var h = {name: "sheprata", isFemale: true};
-
-
-
-//something we dont get in static type language like java and C#, can't assign all kind of value in same variable
