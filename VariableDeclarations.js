68f251a48a4e1bae03c5213883e0935c710f2126
Sheprata Shrestha <i82463@d2hs.com>
2020-11-26 09:20:13 +0545

    Learned about datatypes

--- VariableDeclarations.js
+++ VariableDeclarations.js
@@ -5,16 +5,28 @@
 //something we dont get in static type language like java and C#, can't assign all kind of value in same variable
 
 //Primary type
-var x = 'Sheprata'; //takes single quote as well
-var e = 89;
-var d = "stringtoo";
-var c = true ;
-var y = 2n ** 200n; //large number - bigint is defined with 'n'
-var a = undefined;
-var b = null;
+var a = 'Sheprata'; //takes single quote as well
+var b = 89;
+var c = "stringtoo";
+var d = 345.6;
+var e = true ;
+var f = 2n ** 200n; //large number - bigint is defined with 'n'
+var j = undefined;
+var k = null;
+let b1 = "abc";
+
+//console.log(Number.MAX_VALUE);
+//console.log(Number.MIN_VALUE);
+//var l = +Infinity;
+//var a1 = -Infinity;
+//var a2 = NaN; //not a number;
+//var a3 = "sdfsd"/34;
+//console.log(a3);
 
 //structural type like objects, arrays
 //g - array of number
 var g = [2,3,4,5];
 //h - array of object
 var h = {name: "sheprata", isFemale: true};
+
+console.log(typeof g);
