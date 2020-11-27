b99938de645e86919b48539a9015e4052ac9cf70
Sheprata Shrestha <i82463@d2hs.com>
2020-11-27 09:58:27 +0545

    Learned about Arrow, Closure, Rest Functions

--- Functions.js
+++ Functions.js
@@ -28,3 +28,79 @@ var fun1 = function (number)
 }
 let s1 = fun1(245);
 console.log("Anonymous function : s1 - " + s1);
+
+//Arrow function
+(number) =>  number * number;
+
+//assign to var for arrow fun
+var fun2 = (number) => number * number;
+
+//arrow function - passing arguments in func
+(a,b) => {
+    let sum = a+b;
+    return sum;
+}
+
+//Closure function
+/*public func needs some implementation
+which has private func as well, structional form*/
+//Example 1
+function outside(x) {
+    function inside(y) {
+        return x + y; //preserves x value at first
+    }
+    return inside;
+}
+
+let inside_fn = outside(10); //x
+let result = inside_fn(40); //y
+console.log(result);
+
+// Example 2
+function addSquares(a, b)
+{
+    function sqaure(x) {
+        return x * x;
+    }
+
+    let result = square(a) + square(b);
+    return result;
+}
+
+let r1 = addSquares(3453, 342);
+
+//Example 3
+let createPet = function(name) {
+    return {
+        setName: newName => name = newName,
+       /* setName: function (newName) {
+            name = newName;
+        },*/
+        getName: () => name
+      /*  getName : function(){
+            return name;*/
+        }
+    }
+
+let dog = createPet('diamond');
+
+//Parameters
+function formatName(salutation, name, address){
+  let completeName = salutation + ", " + name + ", " + address;
+  return completeName;
+}
+
+let n = formatName("Miss","Sheprata","Kathmandu");
+let n1 = formatName("Sheprata","ktm");
+console.log(n1);
+
+//Rest function - ...moreArgs stores in array 
+
+function add(a,b, ...moreArgs){
+    console.log(a);
+    console.log(b);
+    console.log(moreArgs);
+}
+
+add(23,25);
+add(23,25,56,57,78);
