a6cdd9847fcee8c2a8fe2d8746f6d2cd1bdd3369
Sheprata Shrestha <i82463@d2hs.com>
2020-12-10 12:48:24 +0545

    Updated exercises.js

--- Exercises.js
+++ Exercises.js
@@ -38,3 +38,59 @@ function numberRange(k,j,range=10) {
     }
 }
 numberRange(2,4,5);
+
+const people = [
+    { name: 'Sheprata', dob: '234234', isMale: false},
+    { name: 'Swechchha', dob: '234544', isMale: true},
+];
+
+//todo
+//console.table(people);
+//process.write('dd);
+//ProcessingInstruction.name('ss');
+
+
+//write a string variable with pragrph as value
+//a no. of sentences, words and word characters
+//no. of special characters
+//no. of vowels and consonants
+//str.charAt(i) === " "
+
+let pragrph = `The fes!tival is ob@served for a fortnight.
+The first nine days are called Navaratri. 
+The goddess Durga is worshiped on these days.
+People worship the image of the goddess in their houses.
+They also visit the temple of the gods and goddess.`;
+let pragrphObj = new String(pragrph);
+
+function countSentences(pragrph){
+  console.log(`Number of sentences splited by decimal are ${(pragrphObj.split(".").length-1)}`);
+  console.log(`Number of words splited by space are ${(pragrphObj.split(" ").length)}`);
+  //let word  = new String(pragrph);
+  let word = pragrphObj.split(" ");
+  const wordarray = Array.from(word);
+  //console.log(`no. of words ${wordarray}`);
+    for (let i = 0; i < word.length; i++) {
+    console.log(`In ${i+1} position word : there are ${wordarray[i].length} characters`);
+    }
+}
+
+countSentences(pragrph);
+
+
+function countSpecialCharacters(pragrph){
+    let specialchar = ["@","!","."];
+    let count = 0;
+    for(let i = 0; i < pragrph.length; i++){
+        if(pragrph.includes(specialchar[i])){
+          console.log(`There is special charc ${specialchar[i]} `);
+        };
+        count++;
+    }
+}
+
+  countSpecialCharacters(pragrph);
+
+ // function countVowelsConsonants(pragrph){
+    
+  //}
